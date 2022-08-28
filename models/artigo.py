45390d3f7a5a37e449b1220df1247007b460b7c4
mielesantos <mielesnts@gmail.com>
2022-08-28 20:12:28 -0300

    add rotas para o curso

--- models/artigo.py
+++ models/artigo.py
@@ -11,6 +11,4 @@ class ArtigoModel(settings.DBBaseModel):
     url_fonte = Column(String(256))
     descricao = Column(String(256))
     usuario_id = Column(Integer, ForeignKey("usuarios.id"))
-    criador = relationship(
-        "UsuarioModel", back_populates="artigos"
-    )  # , lay="joined"
+    criador = relationship("UsuarioModel", back_populates="artigos")  # , lay="joined"
