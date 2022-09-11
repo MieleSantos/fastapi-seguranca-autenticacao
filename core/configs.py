dcb176570907ce6650865347ff010caa25819bb2
mielesantos <mielesnts@gmail.com>
2022-09-10 21:08:10 -0300

    add setup.cfg

--- core/configs.py
+++ core/configs.py
@@ -1,4 +1,4 @@
-import os
+# import os
 
 from pydantic import BaseSettings
 from sqlalchemy.ext.declarative import declarative_base
@@ -14,13 +14,12 @@ class Settings(BaseSettings):
     """
 
     API_V1_STR: str = "/api/v1"
-    DB_URL: str = (
-        "postgresql+asyncpg://postgres:postgres@localhost:5432/faculdade"
-    )
+    DB_URL: str = "postgresql+asyncpg://postgres:postgres@localhost:5432/faculdade"
     DBBaseModel = declarative_base()
 
-    JWT_SECRET: str = os.environ["JWT_SECRET"]
+    # JWT_SECRET: str = os.environ["JWT_SECRET"]
 
+    JWT_SECRET: str = "jfdkśkfiejfkdjsfoijpdf"
     ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 60 * 24 * 7
 
